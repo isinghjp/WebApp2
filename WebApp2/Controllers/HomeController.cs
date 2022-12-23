9202191e889e131f018eb81b377a15acaa7cbb55
imjays <jpkumar541@gmail.com>
2022-12-23 14:37:21 +0530

    Add project files.

---
+++ WebApp2/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApp2.Models;
+
+namespace WebApp2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()  //this is action method. result will be IActionResult
+        {
+            return View(); // it is returning view
+        }
+        public IActionResult aboutus()
+
+        {
+            return View();
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ WebApp2/Controllers/StudentController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp2.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [NonAction] // method become non action. its no more public action method.
+        public string ShowDetails()
+        {
+            return "This is ShowDetails action method.";
+        }
+    }
+}
